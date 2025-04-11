deee361d606316638d480ba86d242f87b508b7b6
DISHA SINGH <155617988+dishashersingh@users.noreply.github.com>
2025-04-11 01:57:30 -0700

    Update app.py

--- MedicalAPI/app.py
+++ MedicalAPI/app.py
@@ -18,7 +18,8 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://frontend-pharmaco.onrender.com"],  
+    allow_origins=["*"],
+
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
