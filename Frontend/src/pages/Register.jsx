f9727372d358671f939b06db40bcdbf532680fd3
DISHA SINGH <155617988+dishashersingh@users.noreply.github.com>
2025-04-11 01:22:47 -0700

    Update Register.jsx

--- Frontend/src/pages/Register.jsx
+++ Frontend/src/pages/Register.jsx
@@ -13,7 +13,7 @@ const Register = () => {
     setError(""); 
 
     try {
-      const response = await fetch("/api/auth/register", {
+      const response = await fetch("https://pharma-co-backend.onrender.com/auth/register", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ name, email, password }),
