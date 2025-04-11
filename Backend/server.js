4201a1e11b4d50377887e296ce336874ce2f93ed
DISHA SINGH <155617988+dishashersingh@users.noreply.github.com>
2025-04-11 01:13:53 -0700

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -11,7 +11,10 @@ dotenv.config();
 connectDB();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: 'https://frontend-pharmaco.onrender.com', 
+  credentials: true
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/api/auth", authRoutes);
